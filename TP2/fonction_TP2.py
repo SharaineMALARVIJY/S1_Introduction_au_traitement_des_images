1bd61b8a412dc39e30cd8c2030386ca190c75b26
sharaine.ma@gmail.com <sharaine.ma@gmail.com>
2025-10-04 14:35:21 +0200

    Ex2 done

--- TP2/fonction_TP2.py
+++ TP2/fonction_TP2.py
@@ -36,7 +36,20 @@ def convolution(im, filtre=[1,1,1,1,1,1,1,1,1]):
                 new_a[y, x] = val
     return new_a
 
-def bruit_sel(a, taux_sel=0.05):
+def bruit_sel_1x1(a, taux_sel=0.15):
+    x_a, y_a = a.shape[1], a.shape[0]
+    new_a = np.zeros_like(a)
+
+    for y in range(0, y_a):
+        for x in range(0, x_a):
+                r = rd.random()
+                if r > taux_sel :
+                    new_a[y, x] = a[y, x]
+                else:
+                    new_a[y, x] = np.array([255,255,255])
+    return new_a
+
+def bruit_sel_2x2(a, taux_sel=0.05):
     x_a, y_a = a.shape[1], a.shape[0]
     new_a = np.zeros_like(a)
 
@@ -68,30 +81,39 @@ def filtre_median(a):
                 new_a[y, x][c] = np.sort(matrice)[4]      
     return new_a
 
-def fft(im):
+def mask_clown(i):
+    i[80:95, 15:30]  = 0
+    i[30:45, 100:115] = 0
+    i[45:55, 40:50] = 0
+    i[70:80, 80:90] = 0  
+    return i
+
+def mask_im_noise(i):
+    i[120:130, :65]  = 0
+    i[120:130, 185:] = 0
+    return i
+
+def fft(im, mask=""):
     i = np.fft.fft2(im)
     i = np.abs(i)
     i = np.log(i)
     i = np.fft.fftshift(i)
-    # i[120:130, :65]  = 255
-    # i[120:130, 185:] = 255
+    if mask=="noise":
+        mask_im_noise(i)
+    if mask=="clown":
+        mask_clown(i)
     return i
 
 
-def filtering_im_noise(im):
+def fft_filter(im, mask=""):
     i_noisy = np.fft.fft2(im)
     i_noisy = np.fft.fftshift(i_noisy)
-    i_noisy[120:130, :65]  = 255
-    i_noisy[120:130, 185:] = 255
-    i_noisy = np.fft.ifftshift(i_noisy)
-    i_noisy = np.fft.ifft2(i_noisy)
-    return np.abs(i_noisy)
 
-def filtering_im_noise(im):
-    i_noisy = np.fft.fft2(im)
-    i_noisy = np.fft.fftshift(i_noisy)
-    i_noisy[120:130, :65]  = 255
-    i_noisy[120:130, 185:] = 255
+    if mask=="noise":
+        mask_im_noise(i_noisy)
+    if mask=="clown":
+        mask_clown(i_noisy)
+
     i_noisy = np.fft.ifftshift(i_noisy)
     i_noisy = np.fft.ifft2(i_noisy)
     return np.abs(i_noisy)
@@ -102,39 +124,38 @@ im_clown = np.array(Image.open("../Images_TP/clown.tif"))
 
 im_noisy_lena = np.array(Image.open("../Images_TP/noisy_Lena.png"))
 
-#%% Exercice 1
-im_noisy_lena_maison = bruit_sel(im_lena)
-im_median_1 = filtre_median(im_noisy_lena_maison)
-im_median_2 = filtre_median(im_median_1)
-im_median_3 = filtre_median(im_median_2)
-#%%
-affiche(im_lena, "Image Original de Lena")
-affiche(im_noisy_lena_maison, "Filtre sel fait maison")
 
-plt.figure()
-plt.subplot(1, 3, 1)
-plt.imshow(im_median_1)
-plt.title("Filtre médian 1")
-plt.subplot(1, 3, 2)
-plt.imshow(im_median_2)
-plt.title("Filtre médian 2")
-plt.subplot(1, 3, 3)
-plt.imshow(im_median_3)
-plt.title("Filtre médian 3")
+#Test ici
+if __name__ == "__main__" :
+    #%% Exercice 1
+    
+    im_noisy_lena_maison = bruit_sel_1x1(im_lena)
+    im_median = []
+    im_median.append(filtre_median(im_noisy_lena_maison))
+    im_median.append(filtre_median(im_median[0]))
+    im_median.append(filtre_median(im_median[1]))
 
-#%% Exercice 2
+    affiche(im_lena, "Image Original de Lena")
+    affiche(im_noisy_lena_maison, "Filtre sel fait maison")
 
-affiche_gray(im_noise)
-affiche_gray(fft(im_noise))
-affiche_gray(filtering_im_noise(im_noise))
-
-affiche_gray(im_clown)
-affiche_gray(fft(im_clown))
-
-#%% Exercice 3
+    plt.figure()
+    for i in range(0, 3):
+        affiche(im_median[i], title=f"Filtre médian {i+1}")
 
 
+    #%% Exercice 2
 
+    affiche_gray(im_noise)
+    affiche_gray(fft(im_noise, "noise"))
+    affiche_gray(fft_filter(im_noise, "noise"))
 
+    affiche_gray(im_clown)
+    affiche_gray(fft(im_clown, "clown"))
+    affiche_gray(fft_filter(im_clown, "clown"))
+
+    #%% Exercice 3
+
+    affiche(im_noisy_lena)
+    affiche(im_noisy_lena_maison)
 
 
